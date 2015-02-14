f69a2f7ffa79f5d21dbdf725353b0157a9bc790c
ngparas <nickgp@gmail.com>
2015-02-13 19:55:36 -0600

    finished data exp

--- titanicScript.R
+++ titanicScript.R
@@ -26,7 +26,9 @@ head(df.train)
   #Each column is a list/vector of data
 missing.data <- data.frame(Count = 0, Feature = names(df.train))
 
-#We want to find the 
+#We want to find the number of missing entries per feature
+#So we'll loop through each column
+#Note that each data type has to be handled differently
 for (ii in 1:ncol(df.train)) {
   if (is.numeric(df.train[,ii])==TRUE) {
     missing.data$Count[ii] = sum(is.na(df.train[,ii]))
@@ -35,17 +37,16 @@ for (ii in 1:ncol(df.train)) {
   }
 }
 
-
+#Visualize the missing data
 #compare native barplot to ggplot
 barplot(missing.data$Count, names.arg=missing.data$Feature)
-
 library(ggplot2)
 ggplot(missing.data, aes(x = Feature, y = Count)) +
   geom_bar(stat = "identity") +
   geom_text(aes(title = 'Missing Data',label = sprintf("%.2f%%", Count/nrow(df.train) * 100)), vjust = -.5)
 detach(package:ggplot2)
 
-#Examine the Data we do have
+#Examine the Data we do have, what are the distributions of the columns?
 barplot(table(df.train$Survived),
         names.arg = c("Perished", "Survived"),
         main="Survived (passenger fate)", col="black")
@@ -57,7 +58,10 @@ barplot(table(df.train$Sex), main="Sex (gender)", col="darkviolet")
 #There are multiple ways to visualize each of these things
 hist(df.train$Age, main="Age", xlab = NULL, col="brown")
 plot(density(df.train$Age, na.rm = TRUE))
+summary(df.train$Age)
 
+#Are they normal? Is there a bias? etc.
+#The range and distribution of these features will be important 
 barplot(table(df.train$SibSp), main="SibSp (siblings + spouse aboard)", 
         col="darkblue")
 barplot(table(df.train$Parch), main="Parch (parents + kids aboard)", 
@@ -68,6 +72,7 @@ barplot(table(df.train$Embarked),
         names.arg = c("","Cherbourg", "Queenstown", "Southampton"),
         main="Embarked (port of embarkation)", col="sienna")
 
+#We can visualize more than 1 feature at a time
 #Colored barplot
 counts <- table(df.train$Survived, df.train$Sex)
 #Counts is a 2x2 matrix - observe the coloring in the following barplot
@@ -76,3 +81,29 @@ barplot(counts,
         ylab = "Number of People", 
         main = "survived and deceased between male and female")
 
+#R has a number of other plotting packages that allow for things like 
+#install.packages("vcd")
+library(vcd)
+mosaicplot(df.train$Sex ~ df.train$Survived, 
+           main="Passenger Fate by Gender", shade=FALSE, color=TRUE, 
+           xlab="Sex", ylab="Survived")
+
+#More statistical plot
+boxplot(df.train$Age ~ df.train$Survived, 
+        main="Passenger Fate by Age",
+        xlab="Survived", ylab="Age")
+
+# Handling Missing Data ---------------------------------------------------
+
+#You may recall that Embarked, Cabin, and Age all had missing values
+summary(df.train$Embarked) # Two values are ""
+summary(df.train$Cabin) # 687 are "", 49 are "(other)"
+summary(df.train$Age) # 177 are NA
+
+#We'll ignore Cabin for now, since 77% is missing
+
+#Lets replace the two missing values for Embarked with the most common, S
+df.train$Embarked[which(df.train$Embarked=='')] <- 'S'
+summary(df.train$Embarked)
+df.train$Embarked=droplevels(df.train$Embarked)
+summary(df.train$Embarked)
